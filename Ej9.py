bcff38d3a88f418d9d07e19657381fd3a28a2d5e
Matias <smatiias@gmail.com>
2022-11-09 21:48:52 -0300

    quinto commit

--- Ej9.py
+++ Ej9.py
@@ -1,19 +1,16 @@
 num = int(input("Ingrese un número: "))
-triangulo = []
-# n = int(input("¿Cuántas filas quieres?"))  # filas que quiere el usuario
+
 i = 1  # Fila actual que es el mismo número que el número de columnas que tiene que tener esa fila
 y = 1  # Número que hay que imprimir
 j = 1  # número de columnas que hay en la fila
-frase = ''
-real = ''
+frase = ""
+
 for i in range(1, num + 1):
-    y=1
+    y = 1
     for j in range(1, i + 1):  # z recorre de 1 hasta x. Ejemplo en la 4 (x=4 z=1,2,3,4)
-        frase += ((" ") + (str(y))).
+        frase += ((" ") + (str(y)))
         y = (y + 2)
-len(frase)
-for k in range(1, j)
-    # invertida = frase[::-1]
-    print(frase)
-    # print(invertida)
-    frase = ''
+    invertida = frase[::-1]
+    # print(frase)
+    print(invertida)
+    frase = ""
